f68c5437402a914f1243ffa8b5bf23606a9ce5b2
jayzeng1987 <zengxiaojiang@jayzeng.local>
2016-12-07 23:03:30 +0800

    增加GVObuResult类实现，解决编译链接问题

---
+++ Test/Test/Test/Test.h
@@ -0,0 +1,15 @@
+//
+//  Test.h
+//  Test
+//
+//  Created by 曾晓江 on 2016/12/7.
+//  Copyright © 2016年 JayZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+-(void)sayHello;
+
+@end
---
+++ Test/Test/Test/Test.m
@@ -0,0 +1,17 @@
+//
+//  Test.m
+//  Test
+//
+//  Created by 曾晓江 on 2016/12/7.
+//  Copyright © 2016年 JayZ. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+-(void)sayHello{
+    NSLog(@"Hello world !");
+}
+
+@end
